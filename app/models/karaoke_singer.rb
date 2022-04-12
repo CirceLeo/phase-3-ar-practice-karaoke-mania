801810f8f33fff1821df6264de395e007d84d399
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-12 16:29:24 -0400

    connections mostly working

--- app/models/karaoke_singer.rb
+++ app/models/karaoke_singer.rb
@@ -1,4 +1,28 @@
 class KaraokeSinger < ActiveRecord::Base
     has_many :performances
     has_many :songs, through: :performances
+
+    def sing song
+        Performance.create(karaoke_singer: self, song: song)
+    end
+
+    def buy_drink
+        self.update(number_of_drinks: self.number_of_drinks + 1)
+    end
+
+    def songs_by_artist artist
+        self.songs
+    end
+
+    def drinks_tab price_per_drink
+        self.number_of_drinks * price_per_drink
+    end
+
+    def self.total_table price_per_drink
+        running_tab = 0
+        KaraokeSinger.all.each do |singer|
+            running_tab = running_tab + singer.number_of_drinks
+        end
+        running_tab * price_per_drink
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,19 @@
 class Song < ActiveRecord::Base
     has_many :performances
     has_many :karaoke_singers, through: :performances
+
+    def self.find_by_artist artist_name
+        Song.find_by artist_name: artist_name
+    end
+
+    def number_of_times_sung
+
+        performances = Performance.where(song_id: self.id)
+        if performances
+            return performances.length
+        else
+            return 0
+        end
+
+    end
 end
